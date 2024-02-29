71db7c4740732af3db3e5ff6f0a7a217c86cbf05
Hokkaido <Hokkaido.12307@gmail.com>
2024-02-29 12:37:33 +0800

    Updated Commit

--- src/Song.java
+++ src/Song.java
@@ -1,4 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Song {
 
-    private String
+    private String title;
+    private String artist;
+    private List<String> listeners;
+
+    public Song(String title, String artist){
+        this.title = title;
+        this.artist = artist;
+        this.listeners = new ArrayList<>();
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getArtist(){
+        return artist;
+    }
+    public int howMany(List<String> newListeners) {
+        int newListenerCount = 0;
+        for (String listener : newListeners) {
+            String lowercaseListener = listener.toLowerCase();
+            if (!listeners.contains(lowercaseListener)) {
+                listeners.add(lowercaseListener);
+                newListenerCount++;
+            }
+        }
+        return newListenerCount;
+    }
+
+
 }
